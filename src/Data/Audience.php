368f770429691179abb9696a99a05a5022ae7266
ju5t <m@ju5t.nl>
2026-02-26 08:41:25 +0100

    fix: make audience member_count optional

--- src/Data/Audience.php
+++ src/Data/Audience.php
@@ -11,7 +11,7 @@ final class Audience extends Data
     public function __construct(
         public string $id,
         public string $name,
-        public int $member_count,
+        public ?int $member_count = null,
         public ?int $web_id = null,
     ) {
         //
